ec9fb03fdd0b1d1f6ac7fea4fd4cfa26f475f07c
wkdrns3918 <wkdrns3918@naver.com>
2022-09-21 15:45:25 +0900

    [AOS] feat : android setting layout

--- AOS/InDive/app/src/main/java/com/ssafy/indive/view/mystudio/MyStudioFragment.kt
+++ AOS/InDive/app/src/main/java/com/ssafy/indive/view/mystudio/MyStudioFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.indive.view.mystudio
 
+import androidx.navigation.fragment.findNavController
 import com.ssafy.indive.base.BaseFragment
 import com.ssafy.indive.R
 import com.ssafy.indive.databinding.FragmentMyStudioBinding
@@ -7,6 +8,17 @@ import com.ssafy.indive.databinding.FragmentMyStudioBinding
 class MyStudioFragment : BaseFragment<FragmentMyStudioBinding>(R.layout.fragment_my_studio) {
 
     override fun init() {
+        clickListener()
+    }
+
+    private fun clickListener() {
+        binding.btnEditProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_myStudioFragment_to_editProfileFragment)
+        }
+        binding.imgAddsong.setOnClickListener {
+            findNavController().navigate(R.id.action_myStudioFragment_to_addSongFirstFragment)
+        }
+
 
     }
 }
