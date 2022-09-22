a886059db27532165856cd4729f3a345c0f34f8a
이명범 <mungmnb777@gmail.com>
2022-09-22 23:53:27 +0900

    [S07P22D102-68] feat: 음원 삭제 기능 추가

--- BE/indive/src/main/java/com/ssafy/indive/domain/music/controller/MusicController.java
+++ BE/indive/src/main/java/com/ssafy/indive/domain/music/controller/MusicController.java
@@ -3,6 +3,7 @@ package com.ssafy.indive.domain.music.controller;
 import com.ssafy.indive.domain.music.controller.dto.WebMusicAddRequestDto;
 import com.ssafy.indive.domain.music.controller.dto.WebMusicModifyRequestDto;
 import com.ssafy.indive.domain.music.service.MusicAddService;
+import com.ssafy.indive.domain.music.service.MusicDeleteService;
 import com.ssafy.indive.domain.music.service.MusicModifyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,8 @@ public class MusicController {
 
     private final MusicModifyService musicModifyService;
 
+    private final MusicDeleteService musicDeleteService;
+
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> addMusic(@Validated @ModelAttribute WebMusicAddRequestDto dto) {
         return new ResponseEntity<>(musicAddService.addMusic(dto.convertToServiceDto()), HttpStatus.OK);
@@ -28,5 +31,10 @@ public class MusicController {
     @PutMapping(value = "/{musicSeq}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> modifyMusic(@Validated @ModelAttribute WebMusicModifyRequestDto dto, @PathVariable("musicSeq") long musicSeq) {
         return new ResponseEntity<>(musicModifyService.modifyMusic(musicSeq, dto.convertToServiceDto()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{musicSeq}")
+    public ResponseEntity<?> deleteMusic(@PathVariable("musicSeq") long musicSeq) {
+        return new ResponseEntity<>(musicDeleteService.deleteMusic(musicSeq), HttpStatus.OK);
     }
 }
