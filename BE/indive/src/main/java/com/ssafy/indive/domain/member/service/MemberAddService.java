e7d068356d1a3c729c6376915acae7ce40cfeb67
DESKTOP-KVCQHCD\multicampus <>
2022-09-27 15:41:25 +0900

    [S07P22D102-65] fix: 회원가입 role 추가, 이미지파일 넣기, 로그인 실패하면 false 수정
    회원가입 role 추가, 이미지파일 넣기, 로그인 실패하면 false 수정했습니다.

--- BE/indive/src/main/java/com/ssafy/indive/domain/member/service/MemberAddService.java
+++ BE/indive/src/main/java/com/ssafy/indive/domain/member/service/MemberAddService.java
@@ -1,6 +1,7 @@
 package com.ssafy.indive.domain.member.service;
 
 import com.ssafy.indive.domain.member.entity.Member;
+import com.ssafy.indive.domain.member.entity.enumeration.Role;
 import com.ssafy.indive.domain.member.repository.MemberRepository;
 import com.ssafy.indive.domain.member.service.dto.ServiceMemberAddRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +26,15 @@ public class MemberAddService {
                 .nickname(dto.getNickname())
                 .wallet(dto.getWallet())
                 .profileMessage(dto.getProfileMessage())
+                //기본 ROLE 넣기
+                .role(Role.ROLE_USER)
+                //기본 이미지 추가
+                .imageOrigin("default_profile.png")
+                .imageUuid("default_profile")
+                .backgroundOrigin("default_background.png")
+                .backgroundUuid("default_background")
                 .build();
+
 
 
         //TODO : 프로필 사진 추가해야 합니다
