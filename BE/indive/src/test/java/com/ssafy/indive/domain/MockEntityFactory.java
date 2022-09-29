b2e3efaeafe89cc59f72d99e165da8ba56ab4940
이명범 <mungmnb777@gmail.com>
2022-09-29 16:56:43 +0900

    [S07P22D102] test: 뮤직 컨트롤러 단위 테스트 작성

---
+++ BE/indive/src/test/java/com/ssafy/indive/domain/MockEntityFactory.java
@@ -0,0 +1,63 @@
+package com.ssafy.indive.domain;
+
+import com.ssafy.indive.domain.member.entity.Member;
+import com.ssafy.indive.domain.member.entity.enumeration.Role;
+import com.ssafy.indive.domain.music.entity.Music;
+import com.ssafy.indive.domain.music.entity.MusicLike;
+import com.ssafy.indive.domain.music.entity.Reply;
+import com.ssafy.indive.domain.nft.entity.Nft;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class MockEntityFactory {
+
+    public static Member member() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        return Member.builder()
+                .seq(1L)
+                .email("mungmnb777@gmail.com")
+                .password(encoder.encode("password"))
+                .nickname("명범짱")
+                .notice("공지사항")
+                .profileMessage("프로필 상태 메시지")
+                .role(Role.ROLE_USER)
+                .wallet("지갑")
+                .build();
+    }
+
+    public static Music music() {
+        return Music.builder()
+                .title("제목")
+                .author(MockEntityFactory.member())
+                .composer("작곡가")
+                .lyricist("작사가")
+                .genre("장르")
+                .description("설명")
+                .lyrics("가사")
+                .build();
+    }
+
+    public static MusicLike musicLike() {
+        return MusicLike.builder()
+                .music(music())
+                .liker(member())
+                .build();
+    }
+
+    public static Reply reply() {
+        return Reply.builder()
+                .music(music())
+                .author(member())
+                .content("댓글")
+                .build();
+    }
+
+    public static Nft nft() {
+        return Nft.builder()
+                .lowerDonationAmount(1000)
+                .stock(100)
+                .artist(member())
+                .build();
+    }
+}
