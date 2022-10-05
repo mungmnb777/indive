dec570d907050595f7f0eeac7a6e74672a541600
taxfdi6371 <taxfdi6371@gmail.com>
2022-10-05 15:53:59 +0900

    feat[S07P22D102-91] feat : 후원 기록 기능 구현 및 UI 수정

--- AOS/InDive/app/src/main/java/com/ssafy/indive/binding/RecyclerBinding.kt
+++ AOS/InDive/app/src/main/java/com/ssafy/indive/binding/RecyclerBinding.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.indive.blockchain.DonationHistory
 import com.ssafy.indive.model.response.MusicDetailResponse
 import com.ssafy.indive.utils.Result
 import com.ssafy.indive.utils.setArtist
@@ -11,6 +12,7 @@ import com.ssafy.indive.view.genre.genrelist.GenreListAdapter
 import com.ssafy.indive.view.home.MusicChartAdapter
 import com.ssafy.indive.view.home.RecentMusicAdapter
 import com.ssafy.indive.view.home.search.SearchArtistAdapter
+import com.ssafy.indive.view.more.mywallet.MyWalletAdapter
 
 object RecyclerBinding {
 
@@ -38,7 +40,10 @@ object RecyclerBinding {
                     val artistList = setArtist(result.data as List<MusicDetailResponse>)
 
                     (view.adapter as ListAdapter<Any, *>).submitList(artistList)
+                }
 
+                is MyWalletAdapter -> {
+                    (view.adapter as ListAdapter<Any, *>).submitList(result.data as List<DonationHistory>)
                 }
 
                 else -> {
--- AOS/InDive/app/src/main/java/com/ssafy/indive/view/more/mywallet/MyWalletViewModel.kt
+++ AOS/InDive/app/src/main/java/com/ssafy/indive/view/more/mywallet/MyWalletViewModel.kt
@@ -2,24 +2,29 @@ package com.ssafy.indive.view.more.mywallet
 
 import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
+import com.ssafy.indive.blockchain.DonationHistory
 import com.ssafy.indive.utils.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.Web3j
-import org.web3j.protocol.admin.Admin
 import javax.inject.Inject
 
 @HiltViewModel
 class MyWalletViewModel @Inject constructor(
     private val web3j: Web3j,
-    private val admin: Admin,
     private val sharedPref: SharedPreferences
 ) : ViewModel() {
     private val _balance: MutableStateFlow<Int> =
         MutableStateFlow(0)
     val balance get() = _balance.asStateFlow()
+
+    private val _donationHistory: MutableStateFlow<Result<List<DonationHistory>>> =
+        MutableStateFlow(Result.Unintialized)
+    val donationHistory get() = _donationHistory.asStateFlow()
 
     fun getTokenBalanceOf(){
         val email = sharedPref.getString(USER_EMAIL, "")
@@ -33,13 +38,13 @@ class MyWalletViewModel @Inject constructor(
         _balance.value = balance
     }
 
-    fun getDonationHistory(){
+    fun getHistory(){
         val email = sharedPref.getString(USER_EMAIL, "")
         val encryptedPrivateKey = sharedPref.getString(email, "")
 
         val privateKey = decrypt(encryptedPrivateKey!!)
         val credential = Credentials.create(privateKey)
 
-        val history = web3j.getDonationHistoryList(privateKey, credential.address)
+        _donationHistory.value = Result.Success((web3j.getDonationHistoryList(privateKey, credential.address)))
     }
 }
