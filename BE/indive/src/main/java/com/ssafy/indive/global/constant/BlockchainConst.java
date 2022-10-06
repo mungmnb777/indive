c17b05232d6549abe26d740d6eaecb4fd0503247
taxfdi6371 <taxfdi6371@gmail.com>
2022-10-07 03:34:52 +0900

    fix : 새 네트워크로 변경 0334

--- BE/indive/src/main/java/com/ssafy/indive/global/constant/BlockchainConst.java
+++ BE/indive/src/main/java/com/ssafy/indive/global/constant/BlockchainConst.java
@@ -12,9 +12,9 @@ public class BlockchainConst {
     public static final String ADMIN_PASSWORD = "ssafy102";
 
     //컨트랙트 주소
-    public static final String INDIVENFT_ADDRESS = "0x34daE5b33A9c7EB365765E009E45c591B64f1688";
-    public static final String INDIVETOKEN_ADDRESS = "0xA8755d02e0A6C79c33dFE5153bd96AE72dA2F11c";
-    public static final String INDIVE_ADDRESS = "0x34daE5b33A9c7EB365765E009E45c591B64f1688";
+    public static final String INDIVENFT_ADDRESS = "0x3CCc77fca952f9A8Eff86226d30927a43F87939a";
+    public static final String INDIVETOKEN_ADDRESS = "0xE41EDa1eC37ed84AC607ee5f6BC999918c39e028";
+    public static final String INDIVE_ADDRESS = "0x6565Bed7cdA813Ff9B4cA5ebe490CE605d5eC415";
 
     public static final long TX_END_CHECK_DURATION = 3000L;
     public static final int TX_END_CHECK_RETRY = 3;
