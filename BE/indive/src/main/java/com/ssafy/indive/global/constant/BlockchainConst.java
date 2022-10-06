b308149d4f5ce23430552bf1ce66f0ba7f83f368
taxfdi6371 <taxfdi6371@gmail.com>
2022-10-07 07:15:30 +0900

    fix : 새 네트워크로 변경 0715

--- BE/indive/src/main/java/com/ssafy/indive/global/constant/BlockchainConst.java
+++ BE/indive/src/main/java/com/ssafy/indive/global/constant/BlockchainConst.java
@@ -7,14 +7,14 @@ public class BlockchainConst {
     public static final long CHAIN_ID = 102L;
 
     //어드민 정보
-    public static final String ADMIN_PRIVATE_KEY = "5f68e6367b07716a301253a6c44cd03fd3b6f885be87db9e96a6239c2fcac6ec";
-    public static final String ADMIN_ADDRESS = "0xa674e936a8f5481d5f8085b481e6c422336b48d6";
+    public static final String ADMIN_PRIVATE_KEY = "b6f60c49e72d48d55319547dbdd8a576c5483120837f127416f7c588c338497a";
+    public static final String ADMIN_ADDRESS = "0x1be93e344660f4d187ec960029f90223b409a713";
     public static final String ADMIN_PASSWORD = "ssafy102";
 
     //컨트랙트 주소
-    public static final String INDIVENFT_ADDRESS = "0x6b8E03c64FF18cbd286418507ea2Db895b6C4b8C";
-    public static final String INDIVETOKEN_ADDRESS = "0x91E88aF88E40B334827277e2CdA752C2d27A6d2d";
-    public static final String INDIVE_ADDRESS = "0x0aBAAA2bA36331120ed13e018bAaA44b3471D85d";
+    public static final String INDIVENFT_ADDRESS = "0x350ea279a34EE46438C32A2b944De98C02900303";
+    public static final String INDIVETOKEN_ADDRESS = "0x480A110BFe0CA4C384aAAfd2FFCE39541a0224E9";
+    public static final String INDIVE_ADDRESS = "0x7E0DADceB1637274B495D65D16e75820F842cb55";
 
     public static final long TX_END_CHECK_DURATION = 3000L;
     public static final int TX_END_CHECK_RETRY = 3;
