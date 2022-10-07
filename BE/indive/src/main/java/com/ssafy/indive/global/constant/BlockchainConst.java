bf91fbbfddf8842ad564ccbd308a47f8bb91f37c
taxfdi6371 <taxfdi6371@gmail.com>
2022-10-07 09:55:48 +0900

    fix : 기존 서버로 복구

--- BE/indive/src/main/java/com/ssafy/indive/global/constant/BlockchainConst.java
+++ BE/indive/src/main/java/com/ssafy/indive/global/constant/BlockchainConst.java
@@ -3,18 +3,18 @@ package com.ssafy.indive.global.constant;
 public class BlockchainConst {
 
     //블록체인 네트워크
-    public static final String BLOCKCHAIN_URL = "http://35.78.241.114:8545";
+    public static final String BLOCKCHAIN_URL = "http://j7d1021.p.ssafy.io:8545";
     public static final long CHAIN_ID = 102L;
 
     //어드민 정보
-    public static final String ADMIN_PRIVATE_KEY = "b6f60c49e72d48d55319547dbdd8a576c5483120837f127416f7c588c338497a";
-    public static final String ADMIN_ADDRESS = "0x1be93e344660f4d187ec960029f90223b409a713";
+    public static final String ADMIN_PRIVATE_KEY = "d8c4538badc5305f045d951ebd5b1d248aa46810dcea7cdaa7f315e5d981b146";
+    public static final String ADMIN_ADDRESS = "0x563fceaa6aec3fa6782787b615a574f20b3d62de";
     public static final String ADMIN_PASSWORD = "ssafy102";
 
     //컨트랙트 주소
-    public static final String INDIVENFT_ADDRESS = "0x350ea279a34EE46438C32A2b944De98C02900303";
-    public static final String INDIVETOKEN_ADDRESS = "0x480A110BFe0CA4C384aAAfd2FFCE39541a0224E9";
-    public static final String INDIVE_ADDRESS = "0x7E0DADceB1637274B495D65D16e75820F842cb55";
+    public static final String INDIVENFT_ADDRESS = "0xC6B48b25B5E057D61790fC97DE787E29Ae002749";
+    public static final String INDIVETOKEN_ADDRESS = "0x02381F301aA9EB7a7AAFDE8fF70f7B6aeC33dd3f";
+    public static final String INDIVE_ADDRESS = "0xec36EbB93B466EB3252a12937B24FdfEaCe90c60";
 
     public static final long TX_END_CHECK_DURATION = 3000L;
     public static final int TX_END_CHECK_RETRY = 3;
